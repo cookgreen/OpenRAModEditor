50d8afa831368a0e6e5a8806861f0a87b52c1cb1
Cook Green <dell20091234@gmail.com>
2020-05-08 11:25:09 +0800

    Adjust SoureCode Folder

---
+++ ORA/OraSDK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRAModEditor.ORA
+{
+	public class OraSDK
+	{
+		private string version;
+		private string fullPath;
+		public string Version { get { return version; } }
+		public string FullPath { get { return fullPath; } }
+		public OraMod Mod { get; set; }
+		public OraSDK(string version, string fullPath)
+		{
+			this.version = version;
+			this.fullPath = fullPath;
+		}
+
+		public void Run()
+		{
+
+		}
+	}
+}
